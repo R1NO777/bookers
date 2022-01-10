c2d3b4037e10564ef98ad66d84e67567af80c808
EC2 Default User <ec2-user@ip-172-31-28-30.ec2.internal>
2022-01-10 11:46:43 +0000

    [Finish]2-9

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'homes#top'
-  resources :todolist
-  delete 'todolist/:id' => 'todolist#destroy', as: 'destroy_todolist'
+  resources :books
+  #delete 'todolist/:id' => 'todolist#destroy', as: 'destroy_todolist'
 end
